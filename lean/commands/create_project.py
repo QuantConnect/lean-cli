fda94749e8d0ef792bb38f2a580d36720cad4af2
Jasper van Merle <jaspervmerle@gmail.com>
2021-06-17 23:08:03 +0200

    Update default project templates

--- lean/commands/create_project.py
+++ lean/commands/create_project.py
@@ -28,18 +28,19 @@ from QuantConnect.Algorithm import QCAlgorithm
 
 class $NAME$(QCAlgorithm):
     def Initialize(self):
-        self.SetStartDate(2020, 8, 2)  # Set Start Date
+        self.SetStartDate(2013, 10, 7)  # Set Start Date
+        self.SetEndDate(2013, 10, 11)  # Set End Date
         self.SetCash(100000)  # Set Strategy Cash
-        # self.AddEquity("SPY", Resolution.Minute)
+        self.AddEquity("SPY", Resolution.Minute)
 
     def OnData(self, data):
         """OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
             Arguments:
                 data: Slice object keyed by symbol containing the stock data
         """
-        # if not self.Portfolio.Invested:
-        #     self.SetHoldings("SPY", 1)
-        #     self.Debug("Purchased Stock")
+        if not self.Portfolio.Invested:
+            self.SetHoldings("SPY", 1)
+            self.Debug("Purchased Stock")
 '''.strip() + "\n"
 
 DEFAULT_PYTHON_NOTEBOOK = """
@@ -111,20 +112,21 @@ namespace QuantConnect.Algorithm.CSharp
     {
         public override void Initialize()
         {
-            SetStartDate(2020, 8, 2); // Set Start Date
+            SetStartDate(2013, 10, 7); // Set Start Date
+            SetEndDate(2013, 10, 11); // Set Start Date
             SetCash(100000); // Set Strategy Cash
-            // AddEquity("SPY", Resolution.Minute);
+            AddEquity("SPY", Resolution.Minute);
         }
 
         /// OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
         /// Slice object keyed by symbol containing the stock data
         public override void OnData(Slice data)
         {
-            // if (!Portfolio.Invested)
-            // {
-            //     SetHoldings("SPY", 1);
-            //     Debug("Purchased Stock");
-            // }
+            if (!Portfolio.Invested)
+            {
+                SetHoldings("SPY", 1);
+                Debug("Purchased Stock");
+            }
         }
     }
 }
