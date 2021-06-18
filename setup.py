3c9fe70d5bf62e65a222a9a0d351cb772fdb1cb0
Jasper van Merle <jaspervmerle@gmail.com>
2021-06-18 04:14:40 +0200

    Auto-update stubs when updating CLI and add project links

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
+# QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+# Lean CLI v1.0. Copyright 2021 QuantConnect Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
 import os
 import re
+import urllib.request
 
 from setuptools import find_packages, setup
 
@@ -17,6 +32,15 @@ def get_version() -> str:
     return version_match.group(1)
 
 
+def get_stubs_version_range() -> str:
+    try:
+        response = urllib.request.urlopen("https://pypi.org/pypi/quantconnect-stubs/json").read()
+        latest_version = json.loads(response)["info"]["version"]
+        return f">={latest_version}"
+    except:
+        return ""
+
+
 # Production dependencies
 install_requires = [
     "click~=7.1.2",
@@ -30,7 +54,7 @@ install_requires = [
     "lxml~=4.6.3",
     "maskpass~=0.3.5",
     "setuptools",
-    "quantconnect-stubs"
+    f"quantconnect-stubs{get_stubs_version_range()}"
 ]
 
 setup(
@@ -39,7 +63,7 @@ setup(
     description="A CLI aimed at making it easier to run QuantConnect's LEAN engine locally and in the cloud",
     author="QuantConnect",
     author_email="support@quantconnect.com",
-    url="https://github.com/QuantConnect/lean-cli",
+    url="https://lean.io/cli",
     long_description=read("README.md").replace("](lean", "](https://github.com/QuantConnect/lean-cli/blob/master/lean"),
     long_description_content_type="text/markdown",
     packages=find_packages(include=["lean", "lean.*"]),
@@ -61,5 +85,9 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9"
-    ]
+    ],
+    project_urls={
+        "Documentation": "https://www.lean.io/docs/lean-cli/getting-started/lean-cli",
+        "Source": "https://github.com/QuantConnect/lean-cli"
+    },
 )
