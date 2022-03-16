09d61d838466f7c10f0d25675589ce59e8bf3e59
Ronit Jain <rjra2611@gmail.com>
2022-03-16 17:33:35 +0530

    initial commit

--- lean/constants.py
+++ lean/constants.py
@@ -99,6 +99,9 @@ BINANCE_PRODUCT_ID = 176
 # The product id of the SAMCO module
 SAMCO_PRODUCT_ID = 173
 
+# The product id of the Interactive Brokers module
+INTERACTIVE_BROKERS_PRODUCT_ID = 1111
+
 # The product ids for which a valid subscription is seen as a valid GUI module subscription
 GUI_PRODUCT_SUBSCRIPTION_IDS = [119, 120]
 
