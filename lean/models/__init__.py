e1163bf798e1e6ea60581bbcdcac30833fb1ab7c
Ronit Jain <rjra2611@gmail.com>
2022-08-11 20:07:32 +0530

    update modules.json version

--- lean/models/__init__.py
+++ lean/models/__init__.py
@@ -17,7 +17,7 @@ import requests
 from pathlib import Path
 
 json_modules = {}
-file_name = "modules-1.2.json"
+file_name = "modules-1.3.json"
 dirname = os.path.dirname(__file__)
 file_path = os.path.join(dirname, f'../{file_name}')
 
