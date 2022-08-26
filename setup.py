ab77da0794d4ef813ddd0e5844dc3b375872e31c
Ronit Jain <rjra2611@gmail.com>
2022-08-26 22:47:24 +0530

    deprecate python 3.6, add python 3.10

--- setup.py
+++ setup.py
@@ -79,17 +79,17 @@ setup(
         "console_scripts": ["lean=lean.main:main"]
     },
     install_requires=install_requires,
-    python_requires=">= 3.6",
+    python_requires=">= 3.7",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Intended Audience :: Financial and Insurance Industry",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9"
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10"
     ],
     project_urls={
         "Documentation": "https://www.lean.io/docs/lean-cli/key-concepts/getting-started",
