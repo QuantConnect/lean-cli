a1a173af5ab6cd89304c0ef8e68707c87ecdf90c
LouisSzeto <hke0073@icloud.com>
2022-09-14 22:41:49 +0800

    remove unnecessary import

--- tests/commands/test_backtest.py
+++ tests/commands/test_backtest.py
@@ -11,7 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from datetime import datetime
 import json
 from pathlib import Path
 from unittest import mock
