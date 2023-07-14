d9daac9834d4f7a8702672503ee689f0220184c4
Jhonathan Abreu <jdabreu25@gmail.com>
2023-07-14 14:15:58 -0400

    Minor change

--- lean/commands/cloud/live/deploy.py
+++ lean/commands/cloud/live/deploy.py
@@ -306,13 +306,7 @@ def deploy(project: str,
             raise RuntimeError(f"Custom portfolio holdings setting is not available for {brokerage_instance.get_name()}")
 
     else:
-        environment_name = "lean-cli-cloud"
-        project_path = Path(cloud_project.name)
-        if project_path.exists():
-            algorithm_file = container.project_manager.find_algorithm_file(project_path)
-            lean_config = container.lean_config_manager.get_complete_lean_config(environment_name, algorithm_file, None)
-        else:
-            lean_config = container.lean_config_manager.get_lean_config()
+        lean_config = container.lean_config_manager.get_lean_config()
         brokerage_instance = _configure_brokerage(lean_config, logger, kwargs, show_secrets=show_secrets)
         live_node = _configure_live_node(logger, api_client, cloud_project)
         notify_order_events, notify_insights, notify_methods = _configure_notifications(logger)
