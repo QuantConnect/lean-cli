92861ffcb472bc5801469b4cf7b7660383c1b22f
Jhonathan Abreu <jdabreu25@gmail.com>
2024-01-23 14:43:34 -0400

    Update development version to conform to SemVer

--- lean/__init__.py
+++ lean/__init__.py
@@ -14,4 +14,4 @@
 # The version is always set to "dev" in the Git repository. When a new release is ready,
 # a maintainer will push a new Git tag which will trigger GitHub Actions to publish a new
 # package to PyPI with the version of the tag.
-__version__ = "dev"
+__version__ = "0.0.0-dev"
